ea8f0f12d50e3230f42bb6e784a7513f4500b01b
Santiago Acosta <168051867+SorathDem@users.noreply.github.com>
2025-10-06 09:17:25 -0500

    Revert "Developer santiago"

--- Dtos/LoginDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UniRumbo.Dtos
-{
-    public class LoginDto
-    {
-        public string Correo { get; set; } = string.Empty;
-        public string Contrasena { get; set; } = string.Empty;
-    }
-}
